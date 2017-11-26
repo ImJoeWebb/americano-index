2210c5d8d71eece0c2d79bb2bffacb059cdf1ca8
Joe Webb <joe.webb@solarisbank.de>
2017-11-25 20:19:47 -0800

    Move core tests out of doctests

--- apps/core/lib/core.ex
+++ apps/core/lib/core.ex
@@ -5,23 +5,12 @@ defmodule Core do
 
   @doc """
   Add a cafe.
-
-  ## Examples
-
-      iex> Core.add_cafe(%{name: "Café Aroma", americano_price: 4_00})
-      :ok
-
   """
 
   defdelegate add_cafe(cafe_to_be_added), to: Storage
 
   @doc """
   List cafes.
-
-  ## Examples
-
-      iex> Core.list_cafes
-      []
   """
 
   defdelegate list_cafes, to: Storage
--- apps/core/test/core_test.exs
+++ apps/core/test/core_test.exs
@@ -1,4 +1,14 @@
 defmodule CoreTest do
   use ExUnit.Case
   doctest Core
+
+  test "add_cafe" do
+    assert :ok == Core.add_cafe(%{name: "Café Aroma", americano_price: 4_00})
+  end
+
+  test "list_cafes" do
+    assert [] == Core.list_cafes
+    :ok = Core.add_cafe(%{name: "Café Aroma", americano_price: 4_00})
+    assert [%{name: "Café Aroma", americano_price: 4_00}] == Core.list_cafes
+  end
 end
